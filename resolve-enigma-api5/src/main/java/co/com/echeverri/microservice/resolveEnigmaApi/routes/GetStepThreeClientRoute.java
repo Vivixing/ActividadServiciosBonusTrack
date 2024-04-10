a0d53bb4c44f61cadc032eb1d9659c93da4f2ed4
Vivi <114113717+Vivixing@users.noreply.github.com>
2024-04-09 19:10:09 -0500

    Update GetStepThreeClientRoute.java

--- resolve-enigma-api5/src/main/java/co/com/echeverri/microservice/resolveEnigmaApi/routes/GetStepThreeClientRoute.java
+++ resolve-enigma-api5/src/main/java/co/com/echeverri/microservice/resolveEnigmaApi/routes/GetStepThreeClientRoute.java
@@ -17,16 +17,13 @@ public class GetStepThreeClientRoute extends RouteBuilder{
 		.routeId("GetStepThree")
 		.setHeader(Exchange.HTTP_METHOD,constant("POST"))
 		.setHeader(Exchange.CONTENT_TYPE,constant("application/json"))
-		/*.setBody(simple("{\n  \"data\": [\n    {\n      \"header\": {\n        \"id\": \"12345\",\n        \"type\": \"TestGiraffeRefrigerator\"\n      },\n      \"enigma\": \"How to put a giraffe into a refrigerator?\"\n    }\n  ]\n}"))*/
 		.hystrix()
 		.hystrixConfiguration().executionTimeoutInMilliseconds(2000).end()
 		.to("freemarker:templates/GetStepThreeClientTemplate.ftl")
-		//.log("Request microservice step three ${body}")
 		.to("http4://localhost:8082/v1/getOneEnigma/getStep")
 		.convertBodyTo(String.class)
 		.log("String response microservice step three ${body}")
 		.unmarshal().json(JsonLibrary.Jackson,ClientJsonApiBodyResponseSuccess.class)
-		//.log("Java response microservice step three ${body}")
 		.process(new Processor() {
 			
 			@Override
@@ -34,8 +31,6 @@ public class GetStepThreeClientRoute extends RouteBuilder{
 				ClientJsonApiBodyResponseSuccess stepOneResponse = (ClientJsonApiBodyResponseSuccess)exchange.getIn().getBody();
 				if (stepOneResponse.getData().get(0).getAnswer().equalsIgnoreCase("Step3: Close the door")) {
 					exchange.setProperty("Step3", stepOneResponse.getData().get(0).getAnswer());
-					//exchange.setProperty("Error", "0000");
-					//exchange.setProperty("descError", "No error");
 				}else {
 					exchange.setProperty("Error", "0001");
 					exchange.setProperty("descError", "Step three is not valid");
